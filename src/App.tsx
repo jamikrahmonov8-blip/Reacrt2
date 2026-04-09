63581e7933ac4071220a9003685e172d1af6b51a
Jamik <jamik@jamshed.com>
2026-04-09 16:13:09 +0500

    new1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './page/Home'
+import About from './page/About'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <nav style={{ display: "flex", gap: "10px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
